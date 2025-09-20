f31ccf2070ec0186fde1688329edeacc8c01391a
PriccilaLucem <priccilalucem@gmail.com>
2025-09-20 18:58:49 -0300

    feat wall collision

--- src/game/physics/physics.c
+++ src/game/physics/physics.c
@@ -1,5 +1,7 @@
 #include "./physics.h"
 
+void wall_collision(BasicCharater* character, float wall_x_min, float wall_x_max, float wall_z_min, float wall_z_max);
+
 void ApplyFloorPhysics(MainCharater* character) {
     if (!character || !character->bc) return;
     
@@ -9,7 +11,21 @@ void ApplyFloorPhysics(MainCharater* character) {
         character->bc->position_y = MIN_HEIGHT_ABOVE_FLOOR;
         character->bc->velocity_y = 0; 
     }
-    
-    // character->velocity_y -= 0.5f; // Gravidade
-    // character->position_y += character->velocity_y;
+    wall_collision(character->bc, -300.0f, 300.0f, -300.0f, 300.0f);
+}
+
+void wall_collision(BasicCharater* character, float wall_x_min, float wall_x_max, float wall_z_min, float wall_z_max) {
+    if (!character) return;
+
+    if (character->position_x < wall_x_min) {
+        character->position_x = wall_x_min;
+    } else if (character->position_x > wall_x_max) {
+        character->position_x = wall_x_max;
+    }
+
+    if (character->position_z < wall_z_min) {
+        character->position_z = wall_z_min;
+    } else if (character->position_z > wall_z_max) {
+        character->position_z = wall_z_max;
+    }
 }
